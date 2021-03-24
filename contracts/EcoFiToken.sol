b7f719b919eab9773b63fb509a5da4ee3461d359
rand0m-us3r <61414643+rand0m-us3r@users.noreply.github.com>
2021-03-23 19:13:15 -0700

    Update EcoFiToken.sol
    updated error in oppenzepplin path

--- contracts/EcoFiToken.sol
+++ contracts/EcoFiToken.sol
@@ -17,7 +17,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /**
  * @title EcoFo Token (ECO)
